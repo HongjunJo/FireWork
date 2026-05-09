575c0a763a0f4fcb621ee0a8a4b8fa8cb65912be
HongJun Jo <hongjunjo23@gmail.com>
2026-05-09 20:00:13 +0900

    fix: 실행 불가 현상 수정

--- lab/build/x64-debug/cmake_install.cmake
+++ lab/build/x64-debug/cmake_install.cmake
@@ -1,8 +1,8 @@
-# Install script for directory: D:/KMU/Game-Physics/lab
+# Install script for directory: D:/KMU/FireWork/lab
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
-  set(CMAKE_INSTALL_PREFIX "D:/KMU/Game-Physics/lab/install/x64-debug")
+  set(CMAKE_INSTALL_PREFIX "D:/KMU/FireWork/lab/install/x64-debug")
 endif()
 string(REGEX REPLACE "/$" "" CMAKE_INSTALL_PREFIX "${CMAKE_INSTALL_PREFIX}")
 
@@ -35,7 +35,7 @@ endif()
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
 if(CMAKE_INSTALL_LOCAL_ONLY)
-  file(WRITE "D:/KMU/Game-Physics/lab/build/x64-debug/install_local_manifest.txt"
+  file(WRITE "D:/KMU/FireWork/lab/build/x64-debug/install_local_manifest.txt"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
 endif()
 if(CMAKE_INSTALL_COMPONENT)
@@ -51,6 +51,6 @@ else()
 endif()
 
 if(NOT CMAKE_INSTALL_LOCAL_ONLY)
-  file(WRITE "D:/KMU/Game-Physics/lab/build/x64-debug/${CMAKE_INSTALL_MANIFEST}"
+  file(WRITE "D:/KMU/FireWork/lab/build/x64-debug/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
 endif()
